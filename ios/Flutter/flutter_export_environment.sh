6b7e85c810caf051d73bc9d82d0a6b55a3665fd7
Aroon Narayanan <aroon@gatech.edu>
2020-11-30 19:15:31 -0500

    old mac commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/aroonnarayanan/workspaces/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/aroonnarayanan/workspaces/secret_santa"
+export "FLUTTER_TARGET=/Users/aroonnarayanan/workspaces/secret_santa/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/aroonnarayanan/workspaces/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.3.1"
+export "FLUTTER_BUILD_NUMBER=7"
+export "TRACK_WIDGET_CREATION=true"
